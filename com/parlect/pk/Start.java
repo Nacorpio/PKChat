f9a667260954dd8b2a7b391dd543121abaa326e7
Nacorpio <teampavilion@gmail.com>
2014-07-03 01:51:04 +0200

    Initial commit

---
+++ com/parlect/pk/Start.java
@@ -0,0 +1,10 @@
+package com.parlect.pk;
+
+public final class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ com/parlect/pk/command/CommandHandler.java
@@ -0,0 +1,5 @@
+package com.parlect.pk.command;
+
+public final class CommandHandler {
+
+}
